5c758f5c7fc20985a6b5053d57360be18fd58bd5
JeffersonSenaa <jeffersonsena12144@gmail.com>
2025-03-15 23:35:44 -0300

    Criação do Array de amigos e método para adicionar o amigo validando e limpando o campo.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo() {
+    try{
+        //Salva nomes no Array
+        let nomes = []
+
+        let inputnome = document.getElementById("amigo").value;
+
+        if(!inputnome.trim()){
+            throw new Error("Por favor, insira um nome.")
+        }
+        
+        nomes.push(inputnome);
+
+        inputnome = document.getElementById("amigo").value = "";
+
+
+        document.getElementById("listaAmigos").textContent = 
+            `Lista de Amigos: ${nomes.join(", ")}`;
+
+
+    
+    }
+    catch (erro){
+        alert(erro.message);
+    }
+    
+}
